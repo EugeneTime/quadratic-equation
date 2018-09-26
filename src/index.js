8bcab9320547b4b38eef2c30e0a289305f02620c
Eugene <37111251+EugeneTime@users.noreply.github.com>
2018-09-26 16:59:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function solveEquation(equation) {
   // for exapmle: 2 * x^2 - 10 * x + 12
   const parseEquation = equation.split(' ');
   const a = parseEquation[0]; // 2
-  const b = parseEquation[3] + parseEquation[4]; // 10
+  const b = parseEquation[3] + parseEquation[4]; // -10
   const c = parseEquation[7] + parseEquation[8]; // 12
   
   const d = b * b - 4 * a * c; // 4;
