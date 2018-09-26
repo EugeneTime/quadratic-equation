aa2ed1b4a4b1d0d5e69eaa416af34fc4a246132c
EugeneTime <eugenetime@bk.ru>
2018-09-26 16:06:47 +0300

    EugeneTime

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  // for exapmle: 2 * x^2 - 10 * x + 12
+  const parseEquation = equation.split(' ');
+  const a = parseEquation[0]; // 2
+  const b = parseEquation[3] + parseEquation[4]; // 10
+  const c = parseEquation[7] + parseEquation[8]; // 12
+  
+  const d = b * b - 4 * a * c; // 4;
+  
+  const xOne = Math.round((-b + Math.sqrt(d)) / (2 * a)); // 2
+  const xTwo = Math.round((-b - Math.sqrt(d)) / (2 * a)); // 3
+  
+  const solutions = [xOne, xTwo];
+  return solutions.sort((xOne, xTwo) => xOne - xTwo);
 }
